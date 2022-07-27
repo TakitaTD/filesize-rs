1be76db864111280299afe5eee961c6d8c3d195c
Tim Takita <takitatd@gmail.com>
2022-07-27 12:11:21 +1000

    omg a du -sh replacement holy shit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod color;
 use termcolor::{Color, ColorChoice, StandardStream};
 
 #[derive(Parser, Debug)]
-struct CliArgs {
+struct CLIArgs {
     file_path: Option<String>,
 }
 
@@ -12,18 +12,19 @@ fn main() {
     let mut stdout = StandardStream::stdout(ColorChoice::Auto);
     color::set_color(&mut stdout, Color::White);
 
-    let cli_args = CliArgs::parse();
+    let cli_args = CLIArgs::parse();
     let dir = cli_args.file_path.unwrap_or(String::from("."));
     let dir = dir.trim();
 
     if !Path::new(&dir).is_dir() {
         color::set_color(&mut stdout, Color::Red);
-        write!(stdout, "error: ");
+        write!(stdout, "error: ").expect("cannot print to standard output");
         color::set_color(&mut stdout, Color::White);
         writeln!(
             stdout,
             "it seems that the directory you specified, is not a directory! :("
-        );
+        )
+        .expect("cannot print to standard output");
         std::process::exit(1)
     }
 
@@ -33,6 +34,7 @@ fn main() {
             "{} is {:#?} bytes",
             file.as_ref().unwrap().path().display(),
             fs::metadata(file.as_ref().unwrap().path()).unwrap().len()
-        );
+        )
+        .expect("cannot print to standard output");
     }
 }
